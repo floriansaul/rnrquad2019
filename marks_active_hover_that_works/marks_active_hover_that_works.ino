1ad514011bdbabe976056656c25078b73a7dba35
leftyfork27 <leftyfork27@gmail.com>
2019-08-08 09:51:08 -0400

    hover that works

---
+++ marks_active_hover_that_works/marks_active_hover_that_works.ino
@@ -0,0 +1,49 @@
+ //
+// Created by Florian VanKampen on 2019-06-17.
+//
+
+#include <rnrquad.h>
+
+float KP = 0.15;
+float KD = 1.9;
+float KI = 0.05;
+float target = 1;
+float minHoverThrottle = .6;
+//.15
+//1.9
+//0.2
+//.569
+PID dPID;
+
+float throttle = 0;
+
+void setup()
+{
+  baseSetup();
+
+  dPID.begin(target, KP, KI, KD, minHoverThrottle);
+}
+
+void loop()
+{
+  baseLoop();
+
+
+  ControllerState controllerState = getControllerState();
+  SensorState sensorState = getSensorState();
+
+//  dPID.KP = KP + .3 * controllerState.trim0;
+//  dPID.target = target + .1 * controllerState.trim1;
+
+  if (controllerState.throttle > .1 )
+  {
+    controllerState.throttle = constrain(dPID.tstep(sensorState.rangeDown), .3 , .8);
+  }
+
+  else
+  {
+    controllerState.throttle = 0;
+  }
+
+  setControllerState(controllerState);
+}
